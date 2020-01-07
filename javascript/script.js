f64dfb907bc297121dbd29c4330b6be5a758758e
Tsairenkun <Tsairenkun@gmail.com>
2020-01-07 13:23:01 +0800

    phase 4 Random Positioning of bubbles

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 console.log("Game");
 var bubbles = [];
 var squares = [];
-var colours = ['Aqua','DarkMagenta','DarkViolet','SpringGreen'];
+var colours = ['Aqua','DarkMagenta','DarkViolet','SpringGreen', 'deepskyblue','darkslategray','deeppink'];
 var numSquares = 30;
 var lose = false;
 var found = false;
@@ -34,7 +34,7 @@ start.addEventListener("click", playmusic);
 var live = document.createElement("div");
 live.className = "livesDisplay";
 live.innerHTML = `LIVES LEFT: ${livescount}`;
-scoreDis.innerHTML = `Score: ${points}`
+scoreDis.innerHTML = `SCORE: ${points}`
 topleft.appendChild(live);
 
 
@@ -51,27 +51,55 @@ function clickBubble(){
     }
 }
 function randomcolour(){
-  return Math.floor(Math.random() * 5);
+    if(points < 1000){
+  return Math.floor(Math.random() * 6);
+} else {
+    return Math.floor(Math.random() * 8);
+    }
 }
 
 function bubble(){
-    var bubble = document.createElement("div");
 //--------- Styling of bubble-------------------
+    if(points < 1000){
+    var bubble = document.createElement("div");
     bubble.style.borderRadius = "50%";
     bubble.style.height = "100px";
     bubble.style.width = "100px";
     bubble.style.backgroundColor = "deeppink";
     bubble.className = "bubble";
+    if(points < 300){
+       bubble.id = "shake";
+    }
 //---------Random positioning of bubble-----------
     var posx = (Math.random() * 600).toFixed();
     var posy = (Math.random() * 600).toFixed();
-    bubble.style.position = "relative";
+    bubble.style.position = "absolute";
     bubble.style.left = posx + "px";
     bubble.style.top = posy + "px";
 //-------------appending to gamebox-------------
     box.appendChild(bubble);
     bubbles.push(bubble);
-        if(points >= 100){
+}
+if(points >= 100 && points < 300){
+            for (var i = 0; i < 5; i++) {
+            var bubble = document.createElement("div");
+            bubble.style.borderRadius = "50%";
+            bubble.style.height = "100px";
+            bubble.style.width = "100px";
+            bubble.className = "bubble";
+            bubble.id = "shake";
+            bubble.style.display = "inline-block";
+            bubble.style.backgroundColor = colours[randomcolour()];
+            var posx = (Math.random() * 200).toFixed();
+            var posy = (Math.random() * 500).toFixed();
+            bubble.style.position = "relative";
+            bubble.style.left = posx + "px";
+            bubble.style.top = posy + "px";
+            box.appendChild(bubble);
+            bubbles.push(bubble);
+
+        }
+    }else if(points >= 300 && points < 1000){
             for (var i = 0; i < 5; i++) {
             var bubble = document.createElement("div");
             bubble.style.borderRadius = "50%";
@@ -88,6 +116,13 @@ function bubble(){
             box.appendChild(bubble);
             bubbles.push(bubble);
 
+        }
+    } else if (points >= 1000) {
+        for (var i = 0; i < 5; i++){
+        var bubble = document.createElement("div");
+        bubble.className = "bubble";
+        box.appendChild(bubble);
+        bubbles.push(bubble);
         }
     }
 }
@@ -109,6 +144,7 @@ function square(){
         square.style.border = "2px solid black";
         square.style.display = "inline-block";
         square.className = "square";
+        square.id = "square";
 //-------Random positioning of square----------
         var posx = (Math.random() * 200) + i.toFixed();
         var posy = (Math.random() * 400) + i.toFixed();
@@ -130,7 +166,7 @@ function loseCheck(){
         points = 0;
         name = "";
         livescount = 5;
-        scoreDis.innerHTML = `Score: ${points}`
+        scoreDis.innerHTML = `SCORE: ${points}`
         displayhighscore();
         clearGameBox(box);
         checkingUser();
@@ -140,11 +176,6 @@ function loseCheck(){
         live.innerHTML = ``;
         livescount--;
         live.innerHTML = `LIVES LEFT: ${livescount}`;
-        Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Thats not meee!',
-                })
     }
 }
 
@@ -153,16 +184,11 @@ function foundCheck(){
     console.log(this.getAttribute("style").includes("deeppink"));
     if(found && this.getAttribute("style").includes("deeppink")){
                 points = points + phasePoints;
-                scoreDis.innerHTML = `Score: ${points}`;
+                scoreDis.innerHTML = `SCORE: ${points}`;
                 console.log(points);
                 createGame();
             } else if (!this.getAttribute("style").includes("deeppink") && livescount > 0){
                 live.innerHTML = ``;
-                 Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Thats not meee!',
-                })
                 livescount--;
                 live.innerHTML = `LIVES LEFT: ${livescount}`;
             } else if(livescount === 0){
@@ -172,7 +198,7 @@ function foundCheck(){
                     points = 0;
                     name = "";
                     livescount = 5;
-                    scoreDis.innerHTML = `Score: ${points}`
+                    scoreDis.innerHTML = `SCORE: ${points}`
                     displayhighscore();
                     clearGameBox(box);
                     checkingUser();
@@ -200,6 +226,7 @@ function square2(){
         square.style.border = "2px #03e8fc solid";
         square.style.display = "inline-block";
         square.className = `square${i}`;
+        square.id = "square";
 //-------Random positioning of square----------
         var posx = (Math.random() * 100) + i.toFixed();
         var posy = (Math.random() * 500) + i.toFixed();
@@ -257,6 +284,7 @@ function square3(){
         square.style.border = "2px #03e8fc solid";
         square.style.display = "inline-block";
         square.className = `square${i}`;
+        square.id = "square";
 //-------Random positioning of square----------
         var posx = (Math.random() * 100) + i.toFixed();
         var posy = (Math.random() * 200) + i.toFixed();
@@ -337,6 +365,139 @@ var bubblemovement = document.querySelectorAll(".bubble");
     }
 }
 
+
+
+//-----phase 4--------------------
+function square4(){
+    for (var i = 0; i < numSquares; i++) {
+        var square = document.createElement("div");
+//---------Styling of Boxes---------------------
+        square.style.height = "100px";
+        square.style.width = "100px" ;
+        square.style.backgroundColor = colours[randomcolour()];
+        square.style.border = "2px #03e8fc solid";
+        square.style.display = "inline-block";
+        square.className = `square${i}`;
+        square.id = "square";
+//-------Random positioning of square----------
+        var posx = (Math.random() * 100) + i.toFixed();
+        var posy = (Math.random() * 200) + i.toFixed();
+        square.style.position = "relative";
+        square.style.left = posx + "px";
+        square.style.top = posy + "px";
+//-------appending to gamebox-----------
+        box.appendChild(square);
+        squares.push(square);
+    }
+}
+
+function movementPhaseFour(){
+var squaremovementup = [
+  { transform: 'translateX(00px)' },
+  { transform: 'translateY(-450px)' }
+    ];
+
+var squaremovementdown = [
+  { transform: 'translateX(00px)' },
+  { transform: 'translateY(400px)' }
+    ];
+
+var squaremovementside = [
+  { transform: 'translateX(250px)' },
+  { transform: 'translateY(00px)' }
+    ];
+
+var squaremovementdialeft = [
+  { transform: 'translateX(250px)' },
+  { transform: 'translateY(450px)' }
+    ];
+
+
+var squaremovementdiaright = [
+  { transform: 'translateX(-250px)' },
+  { transform: 'translateY(450px)' }
+    ];
+
+var squaretiming = {
+    duration: 1500,
+    iterations: Infinity,
+    direction: 'alternate',
+}
+for (var i = 0; i < squares.length; i++) {
+    if (i < 5 ) {
+        document.querySelector(`.square${i}`).animate(
+        squaremovementdown,
+        squaretiming
+        )
+    }
+}
+for (var i = 5; i < squares.length; i++) {
+    if (i < 10 ) {
+        document.querySelector(`.square${i}`).animate(
+        squaremovementdialeft,
+        squaretiming
+        )
+    }
+}
+
+for (var i = 10; i < squares.length; i++) {
+    if (i < 15 ) {
+        document.querySelector(`.square${i}`).animate(
+        squaremovementdiaright,
+        squaretiming
+        )
+    }
+}
+
+for (var i = 15; i < squares.length; i++) {
+    if (i < 20 ) {
+        document.querySelector(`.square${i}`).animate(
+        squaremovementside,
+        squaretiming
+        )
+    }
+}
+
+for (var i = 20; i < squares.length; i++) {
+    squares[i]
+        document.querySelector(`.square${i}`).animate(
+        squaremovementup,
+        squaretiming
+        )
+    }
+ 
+}
+
+
+const rand = (multi) => {
+return parseInt(multi * Math.random() ,10);
+}
+
+function move(){
+var divs = document.querySelectorAll(".bubble");
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+
+let constraint = Math.min(ww, wh);
+    for (var i = 0; i < divs.length; i++){
+        var div = divs[i];
+        var w = rand(constraint);
+        var x = rand((ww - w));
+        var y = rand((wh - w));
+
+        div.style.width = (w - 500) + 'px';
+        div.style.height = (w - 500) + 'px';
+        div.style.top = y + 'px';
+        div.style.left = x + 'px';
+
+        div.style.transition = (rand(100) + 700) +'ms';
+
+        div.style.background = colours[randomcolour()];
+        div.style.opacity = Math.random() + 0.5;
+        };
+    }
+
+
 //--------Game creation-------------------
 function createGame(){
 if(points < 100){
@@ -356,7 +517,7 @@ if(points < 100){
         square2();
         clickSquare();
         movementPhaseTwo();
-    }else if (points >= 300){
+    }else if (points >= 300 && points < 1000){
             clearGameBox(box);
             bubbles = [];
             squares = [];
@@ -365,7 +526,17 @@ if(points < 100){
             square3();
             clickSquare();
             movementPhaseThree();
-            }
+        }else if (points >= 1000){
+            clearGameBox(box);
+            bubbles = [];
+            squares = [];
+            bubble();
+            clickBubble();
+            square3();
+            clickSquare();
+            movementPhaseFour();
+            window.setInterval(move, 1000);
+        }
 }
 
 
@@ -384,6 +555,54 @@ scoreArray.innerHTML = "";
 
 }
 
+//----- keying in username-----------
+function gettingPlayerName(){
+    var x = document.createElement("INPUT");
+    x.placeholder= "USERNAME";
+    x.className = "username";
+    x.setAttribute("type", "text");
+    document.querySelector(".popup").appendChild(x);
+    x.addEventListener('keypress', function(e){
+        if(e.key === 'Enter'){
+               if(x.value === ''){
+                alertusername();
+               }else{
+                name = x.value;
+                checkingUser();
+               }
+        x.value = '';
+        }
+    })
+}
+
+//---------username Request Pop up-----
+function userpopup(){
+    var popup = document.createElement("div");
+    popup.className = "popup";
+    newname.appendChild(popup);
+    var h = document.createElement("h1");
+    h.innerText = "FIND ME"
+    popup.appendChild(h);
+    gettingPlayerName();
+    blurfilter();
+}
+
+//------- blur function -----------
+function blurfilter(){
+    whole.classList.add("blurfilter");
+}
+
+//----check if user input a username------
+function checkingUser(){
+    if (name === '') {
+        userpopup();
+    }else{
+        whole.classList.remove("blurfilter");
+        newname.removeChild(document.querySelector(".popup"));
+    }
+}
+
+//---------Alerts and sound-----------
 function alert0(){
 Swal.fire({
         icon: 'info',
@@ -416,50 +635,4 @@ function sound(src) {
 
 function playmusic(){
     myMusic.play()
-}
-
-
-//----- keying in username-----------
-function gettingPlayerName(){
-    var x = document.createElement("INPUT");
-    x.placeholder= "USERNAME";
-    x.className = "username";
-    x.setAttribute("type", "text");
-    document.querySelector(".popup").appendChild(x);
-    x.addEventListener('keypress', function(e){
-        if(e.key === 'Enter'){
-               if(x.value === ''){
-                alertusername();
-               }else{
-                name = x.value;
-                checkingUser();
-               }
-        x.value = '';
-        }
-    })
-}
-
-//---------username Request Pop up-----
-function userpopup(){
-    var popup = document.createElement("div");
-    popup.className = "popup";
-    newname.appendChild(popup);
-    gettingPlayerName();
-    blurfilter();
-}
-
-//------- blur function -----------
-function blurfilter(){
-    whole.classList.add("blurfilter");
-}
-
-//----check if user click start without inputing a username------
-
-function checkingUser(){
-    if (name === '') {
-        userpopup();
-    }else{
-        whole.classList.remove("blurfilter");
-        newname.removeChild(document.querySelector(".popup"));
-    }
 }
